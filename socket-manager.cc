b8bd5b324c871aff6f4e0bf33b0b42f7431cf5dd
yonde927 <svyuta1202@gmail.com>
2022-08-24 17:02:03 +0900

    accept thread

--- socket-manager.cc
+++ socket-manager.cc
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <unistd.h>
 
-
 //peer_socket_manager
 peer_socket_manager::peer_socket_manager(string ip, short port){
     self_addr.sin_family = AF_INET;
@@ -12,59 +11,160 @@ peer_socket_manager::peer_socket_manager(string ip, short port){
     server_socket = 0;
 }
 
-int peer_socket_manager::accept_peer_socket(){
+int peer_socket_manager::accept_socket(){
     int tmp_socket,rc,id{0};
     int addr_size = sizeof(sockaddr_in);
     tmp_socket = accept(server_socket, (struct sockaddr*)&self_addr, (socklen_t*)&addr_size);
     if(tmp_socket < 0){
         return -1;
     }
-    //peer_idの受信
-    rc = recv(tmp_socket, &id, sizeof(int), 0);
-    if(rc != 0){
+    return tmp_socket;
+}
+
+int peer_socket_manager::get_new_id(){
+    int id{1};
+   
+    while(true){
+        if(peer_map.find(id) == peer_map.end()){
+            break;
+        }
+        id++;
+    }
+    return id;
+}
+
+int peer_socket_manager::get_waiting_free_ticket(){
+    for(auto i = peer_map.begin(); i != peer_map.end(); ++i){
+        if(i->second->is_new && i->second->is_waiting){
+            return i->first;
+        }
+    }
+    return -1;
+}
+
+int peer_socket_manager::accept_peer(){
+    //条件変数を扱うための排他制御
+    std::lock_guard<mutex> lock(waiting_mtx);
+
+    int tmp_socket = accept_socket();
+    if(tmp_socket < 0){
+        return tmp_socket;
+    }
+    //peer_idを受信
+    int rc,id;
+    if(recv(tmp_socket, &id, sizeof(int), 0) <= 0){
         close(tmp_socket);
         return -1;
     }
 
+    //peer_mapに登録
     if(id == 0){
-        //新規idを要求
-        int new_id{1};
-        while(true){
-            if(fd_map.find(new_id) == fd_map.end()){
-                //fd_mapに登録されていないid
-                break;
-            }
-            new_id++;
-        }
-        id = new_id;
-        //新規idを返信
-        rc = send(tmp_socket, &new_id, sizeof(int), 0);
-        if(rc != 0){
-            close(tmp_socket);
-        }
-        fd_map.insert(std::make_pair(new_id, tmp_socket));
-        return new_id;
-    }else if(id > 0){
-        //再接続を要求
-        //idを返信
-        rc = send(tmp_socket, &id, sizeof(int), 0);
-        if(rc != 0){
+        //待機中で所有者のいないpeer_socket_ticketを取得
+        id = get_waiting_free_ticket();
+        //idを送信、idが0のときはそのままエラー送信
+        if(send(tmp_socket, &id, sizeof(int), 0) <= 0){
             close(tmp_socket);
             return -1;
         }
-        //fd_mapを更新
-        fd_map.insert_or_assign(id, tmp_socket);
-    }else{
-        //接続エラー
-        id = -1;
-        rc = send(tmp_socket, &id, sizeof(int), 0);
+        //更新通知
+        if(id > 0){
+            //待機スレッドに通知
+            auto ticket = peer_map.at(id);
+            ticket->socket_fd = tmp_socket;
+            ticket->cond.notify_one();
+        }
+        return id;
+    }else if(id > 0){
+        //idが有効か確認
+        if(peer_map.find(id) != peer_map.end()){
+            //idが存在する
+            //idが接続待機中なのか確認
+            auto ticket = peer_map.at(id);
+            if(ticket->is_waiting){
+                if(send(tmp_socket, &id, sizeof(int), 0) <= 0){
+                    close(tmp_socket);
+                    return -1;
+                }
+                //ticketにソケットを登録して待機スレッドに通知
+                ticket->socket_fd = tmp_socket; 
+                ticket->cond.notify_one();
+            }
+        }
+        return id;
+    }
+
+    //上の条件にあぶれた無効なidはソケットをエラー送信して切断する
+    id = -1;
+    if(send(tmp_socket, &id, sizeof(int), 0) <= 0){
         close(tmp_socket);
         return -1;
     }
     return id;
 }
 
+int peer_socket_manager::accept_loop(){
+    int id{0};
+    while(true){
+        id = accept_peer();
+        if(id < 0){
+            std::cout << "accept fail" << std::endl;
+        }
+    }
+}
 
+//待機スレッド上で実行されるソケット要求関数
+peer_socket peer_socket_manager::get_peer_socket_server(int peer_id){
+    peer_socket psocket{0};
+    if(peer_id == 0){
+        auto ticket = shared_ptr<peer_socket_ticket>(new peer_socket_ticket);
+        int new_id;
+        //新しいticketを登録する
+        {
+            std::lock_guard<mutex> lock(waiting_mtx);
+            new_id = get_new_id();
+            ticket->peer_id = new_id;
+            peer_map.insert_or_assign(new_id, ticket);
+        }
+        //通知を待ち、peer_socketを取り出す
+        {
+            std::unique_lock<mutex> lock(waiting_mtx);
+            ticket->cond.wait(lock, [&](){return ticket->socket_fd > 0;});
+            ticket->is_new = false;
+            ticket->is_waiting = false;
+            psocket.peer_id = new_id;
+            psocket.socket_fd = ticket->socket_fd;
+        }
+    }else{
+        if(peer_map.find(peer_id) == peer_map.end()){
+            return psocket;
+        }
+        //ticketを登録する
+        auto ticket = peer_map.at(peer_id);
+        {
+            std::lock_guard<mutex> lock(waiting_mtx);
+            //ticketをリロード
+            ticket->socket_fd = 0;
+            ticket->is_new = false;
+            ticket->is_waiting = true;
+        }
+        {
+            std::unique_lock<mutex> lock(waiting_mtx);
+            ticket->cond.wait(lock, [&](){return ticket->socket_fd > 0;});
+            ticket->is_new = false;
+            ticket->is_waiting = false;
+            psocket.peer_id = peer_id;
+            psocket.socket_fd = ticket->socket_fd;
+        }
+    }
+    return psocket;
+}
+
+peer_socket  peer_socket_manager::get_peer_socket_client(int peer_id){
+    peer_socket psocket{0};
+
+}
+
+//サーバー管理
 int peer_socket_manager::start_server(){
     int rc{0};
     int yes{1};
@@ -90,6 +190,7 @@ int peer_socket_manager::start_server(){
     }
 
     //acceptスレッドを起動
+    accept_thread = std::move(thread(&peer_socket_manager::accept_loop, this));
 
     mode = 1;
 
@@ -109,10 +210,6 @@ int peer_socket_manager::stop_server(){
     return 0;
 }
 
-//新規ペアを接続
-int peer_socket_manager::accept_peer(shared_ptr<peer_socket> psocket){
-    return 0;
-}
 
 
 
--- socket-manager.h
+++ socket-manager.h
@@ -9,29 +9,45 @@
 #include <condition_variable>
 #include <memory>
 
-using std::vector, std::map, std::pair, std::string, std::lock_guard, std::mutex, std::condition_variable, std::thread, std::shared_ptr, std::unique_ptr;
+using std::vector, std::map, std::pair, std::string, std::mutex, std::condition_variable, std::thread, std::shared_ptr;
 
+struct peer_socket{
+    int peer_id{0};
+    int socket_fd{0};
+};
+
+struct peer_socket_ticket{
+    //新規peerをacceptする時は、peer_idが0の条件変数をwaitする
+    int peer_id{0};
+    int socket_fd{0};
+    bool is_new{true};
+    bool is_waiting{true};
+    condition_variable cond;
+}
+;
 //ネットワークペアごとに一貫したソケットを提供するための機能
 class peer_socket_manager{
     private:
-        int mode;
-        int server_socket;
-        std::mutex accept_mtx;
-        std::mutex waiting_mtx;
+        int mode{0};
+        int server_socket{0};
+        thread accept_thread;
+        mutex waiting_mtx;
         sockaddr_in self_addr;
-        map<int, int> fd_map;
-        map<int, unique_ptr<condition_variable>> waiting_map;
-        int peer_socket_manager::accept_peer_socket();
+        map<int, shared_ptr<peer_socket_ticket>> peer_map;
+        map<int, sockaddr_in> id_addr_map;
+        
+        int accept_socket();
+        int get_new_id();
+        int get_waiting_free_ticket();
+        int accept_peer();
+        int accept_loop();
+    public:
+        peer_socket_manager(string ip, short port);
+        int start_server();
+        int stop_server();
         peer_socket  get_peer_socket_server(int peer_id);
         peer_socket  get_peer_socket_client(int peer_id);
-    public:
-        struct peer_socket{
-            int peer_id;
-            int socket_fd;
-        };
-        peer_socket_manager(string ip, short port);
-        int peer_socket_manager::start_server();
-        peer_socket  get_peer_socket(int peer_id);
+        peer_socket  get_peer_socket_client(sockaddr_in& addr);
 };
 
 //memo
